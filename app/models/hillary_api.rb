0a39817724d5d0df3e5720abbd3a5174f57566a6
spunyasena <sanjaya@simpolfy.com>
2016-07-31 17:30:13 -0400

    add rescue

--- app/models/hillary_api.rb
+++ app/models/hillary_api.rb
@@ -14,21 +14,24 @@ class HillaryApi
     while i <= pages
       data = self.events(i)
       data["events"].each do |event_info|
-        event = Event.where(id: event_info["id"]).first_or_initialize
-        event.name = event_info["name"]
-        event.description = event_info["description"]
-        event.start_date = event_info["startDate"]
-        event.end_date = event_info["endDate"]
-        if event_info["locations"] and event_info["locations"][0]
-          event.address_1 = event_info["locations"][0]["address1"]
-          event.address_2 = event_info["locations"][0]["address2"]
-          event.city = event_info["locations"][0]["city"]
-          event.state = event_info["locations"][0]["state"]
-          event.zip_code = event_info["locations"][0]["postalCode"]
-          event.latitude = event_info["locations"][0]["latitude"]
-          event.longitude = event_info["locations"][0]["longitude"]
+        begin
+          event = Event.where(id: event_info["id"]).first_or_initialize
+          event.name = event_info["name"]
+          event.description = event_info["description"]
+          event.start_date = event_info["startDate"]
+          event.end_date = event_info["endDate"]
+          if event_info["locations"] and event_info["locations"][0]
+            event.address_1 = event_info["locations"][0]["address1"]
+            event.address_2 = event_info["locations"][0]["address2"]
+            event.city = event_info["locations"][0]["city"]
+            event.state = event_info["locations"][0]["state"]
+            event.zip_code = event_info["locations"][0]["postalCode"]
+            event.latitude = event_info["locations"][0]["latitude"]
+            event.longitude = event_info["locations"][0]["longitude"]
+          end
+          event.save if event.changed? or event.new_record?
+        rescue
         end
-        event.save if event.changed? or event.new_record?
       end
       i+=1
     end
